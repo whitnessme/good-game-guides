6a27587a415568dcc963cce5483e7790d5d1439f
Whitney Minson <whitnessprints@gmail.com>
2022-01-18 13:33:33 -0800

    Create database, user model, and Doug Demo seed file

---
+++ db/migrations/20220118211138-create-user.js
@@ -0,0 +1,37 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      email: {
+        type: Sequelize.STRING(50),
+        allowNull: false,
+        unique: true
+      },
+      fullName: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+      },
+      hashedPassword: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
