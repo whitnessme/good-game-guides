690643198f2388c60489be0e6157b06d048d3636
Laura <laura.maser3@yahoo.com>
2022-01-19 16:40:13 -0500

    Created router for my-game-guides and imported into app.js.

---
+++ routes/my-game-guides.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { check, validationResult } = require("express-validator");
+
+const { csrfProtection, asyncHandler } = require("../utils");
+const db = require("../db/models");
+
+const router = express.Router();
+
+
+
+
+
+module.exports = router;
