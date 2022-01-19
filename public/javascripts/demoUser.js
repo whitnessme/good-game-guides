d67fa231d73c70e8a554ee843b6f868f6bf2dae5
alexsmaldone <alexsmaldone@gmail.com>
2022-01-18 23:35:38 -0500

    added userdemo credentials login js file

---
+++ public/javascripts/demoUser.js
@@ -0,0 +1,15 @@
+function demoUserLogin(e) {
+  let email = document.querySelector("input[name=email]");
+  let password = document.querySelector("input[name=password]");
+
+  if (e.target.checked) {
+    email.value = "demo@demodome.com";
+    password.value = "password";
+  }
+}
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  let checkbox = document.querySelector("#demo-user-box");
+
+  checkbox.addEventListener("change", demoUserLogin);
+});
