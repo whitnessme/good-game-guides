6218d5ea5fdd49a00e763e971a94838049d37866
Whitney Minson <whitnessprints@gmail.com>
2022-01-19 15:53:23 -0800

    fixed coverImg relative paths, added images to home page, added auth check

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,20 @@
 const express = require("express");
+const { user } = require("pg/lib/defaults");
 const router = express.Router();
+const db = require("../db/models");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  const guides = [
-    {title: 'Coming soon', author: "demo"},
-    {title: 'Coming soon', author: "demo"}
-  ]
-  // const guides = await db.GameGuide.findAll()
+router.get("/", async(req, res, next) => {
+  const guides = await db.GameGuide.findAll()
+  const currentGuides = await db.StatusShelf.findAll({
+    where: {
+      userId: user.id,
+      statusId: 2
+    },
+    include: {
+      model: GameGuide
+    }
+  })
   res.render("index", { title: "Home", guides});
 });
 
