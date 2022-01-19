90a576eb38cbc2e58c956d86b8d72f4028ca95e0
Laura <laura.maser3@yahoo.com>
2022-01-19 17:19:44 -0500

    Began working on GET / route in my-game-guides.js.

--- routes/my-game-guides.js
+++ routes/my-game-guides.js
@@ -6,8 +6,16 @@ const db = require("../db/models");
 
 const router = express.Router();
 
+router.get('/', asyncHandler(async (req, res) => {
+    const gameGuides = await db.GameGuide.findAll({ where: {  } });
+    res.render('my-game-guides', { title:  });
+}));
 
+router.get('/status-shelves/:id(\\d+)');
 
+router.get('/custom-shelves/:id(\\d+)');
+
+router.get('/custom-shelves/edit');
 
 
 module.exports = router;
