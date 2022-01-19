fb545b4c0c82238431d7658d28e73785df4f7653
Whitney Minson <whitnessprints@gmail.com>
2022-01-18 16:32:09 -0800

    index files updated for landing page: pug, route, css

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "a/A Express Skeleton Home" });
+  res.render("index", { title: "Good Game Guides" });
 });
-
-// Logout Button
-
-router.post()
 
 module.exports = router;
