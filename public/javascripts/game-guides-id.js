cc950b7d986d46c10db216b5abd15879312b5402
alexsmaldone <alexsmaldone@gmail.com>
2022-01-20 13:46:32 -0500

    updating styling of gameguide status dorpdown menu

--- public/javascripts/game-guides-id.js
+++ public/javascripts/game-guides-id.js
@@ -1,3 +1,14 @@
 window.addEventListener("DOMContentLoaded", (e) => {
+  // display dropdown menu
   let shelfDropDownButton = document.querySelector(".shelf-dropdown-button");
+  function toggleDropDown(e) {
+    let shelfMenu = document.querySelector(".wtp-shelf-menu");
+    if (shelfMenu.style.display !== "none") {
+      shelfMenu.style.display = "none";
+    } else {
+      shelfMenu.style.display = "block";
+    }
+  }
+
+  shelfDropDownButton.addEventListener("click", toggleDropDown);
 });
