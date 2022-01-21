3defb265dcacda5ef5d001e302542d5918308e79
Laura <laura.maser3@yahoo.com>
2022-01-21 14:21:15 -0500

    Created conditionals to have current pages shelf link greyed out.

--- routes/my-game-guides.js
+++ routes/my-game-guides.js
@@ -19,21 +19,23 @@ const router = express.Router();
 
 // /my-game-guides/
 router.get('/', asyncHandler(async (req, res) => {
+    const url = req.url;
     const { userId } = req.session.auth;
+
     const guides = await allStatusShelfEntries(userId);
 
-
-    res.render('my-game-guides', { guides });
+    res.render('my-game-guides', { url, guides });
 }));
 
 // /my-game-guides/status-shelves/:id
 router.get('/status-shelves/:id(\\d+)', asyncHandler(async (req, res) => {
+    const url = req.url;
     const { userId } = req.session.auth;
     const shelfId = parseInt(req.params.id, 10);
 
     const guides = await findStatusShelfEntries(userId, shelfId);
 
-    res.render('my-game-guides', { guides });
+    res.render('my-game-guides', { url, guides });
 }));
 
 // /my-game-guides/custom-shelves/:id
