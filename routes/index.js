3e21d4af5802e6074e00d5b3981f2054d12d51e3
alexsmaldone <alexsmaldone@gmail.com>
2022-01-21 19:04:41 -0500

    commiting stopping point

--- routes/index.js
+++ routes/index.js
@@ -5,42 +5,52 @@ const db = require("../db/models");
 const { csrfProtection, asyncHandler } = require("../utils");
 
 /* GET home page. */
-router.get("/", asyncHandler(async(req, res, next) => {
-  let currentGuides;
-  let wantToGuides;
-  const guides = await db.GameGuide.findAll()
-  let noCurr = []
-  let noWant = []
-  
-  if(req.session.auth) {
-    const { userId } = req.session.auth;
+router.get(
+  "/",
+  asyncHandler(async (req, res, next) => {
+    let currentGuides;
+    let wantToGuides;
+    const guides = await db.GameGuide.findAll();
+    let noCurr = [];
+    let noWant = [];
 
-    currentGuides = await db.StatusShelf.findAll({
-      where: {
-        userId,
-        statusId: 2
-      },
-      include: db.GameGuide
-    })
-    wantToGuides = await db.StatusShelf.findAll({
-      where: {
-        userId,
-        statusId: 1
-      },
-      include: {
-        model: db.GameGuide
-      }
-    })
-  }
+    if (req.session.auth) {
+      const { userId } = req.session.auth;
 
-  if (!currentGuides.length) {
-    noCurr.push("Please add guides to view them here")
-  }
-  console.log(noCurr.length)
-  if (!wantToGuides.length) {
-    noWant.push("Please add guides to view them here")
-  }
-  res.render("index", { title: "Home", guides, currentGuides, wantToGuides, noCurr, noWant});
-}));
+      currentGuides = await db.StatusShelf.findAll({
+        where: {
+          userId,
+          statusId: 2,
+        },
+        include: db.GameGuide,
+      });
+      wantToGuides = await db.StatusShelf.findAll({
+        where: {
+          userId,
+          statusId: 1,
+        },
+        include: {
+          model: db.GameGuide,
+        },
+      });
+    }
+
+    if (!currentGuides.length) {
+      noCurr.push("Please add guides to view them here");
+    }
+    console.log(noCurr.length);
+    if (!wantToGuides.length) {
+      noWant.push("Please add guides to view them here");
+    }
+    res.render("index", {
+      title: "Home",
+      guides,
+      currentGuides,
+      wantToGuides,
+      noCurr,
+      noWant,
+    });
+  })
+);
 
 module.exports = router;
