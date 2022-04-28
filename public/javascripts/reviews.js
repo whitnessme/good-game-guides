25bc67ff634ae62882dd5ab201ef7fcb9896dbed
Laura <laura.maser3@yahoo.com>
2022-04-27 22:34:02 -0400

    Made it so review form has hearts filled in if one is pre-selected.

--- public/javascripts/reviews.js
+++ public/javascripts/reviews.js
@@ -1,12 +1,135 @@
 window.addEventListener('DOMContentLoaded', (e) => {
-
-    // Change background of all previous hearts on hover on new review form
+    // Heart Rating Radio Inputs
     let heart1 = document.getElementById('rating1');
     let heart2 = document.getElementById('rating2');
     let heart3 = document.getElementById('rating3');
     let heart4 = document.getElementById('rating4');
     let heart5 = document.getElementById('rating5');
 
+    // Heart CSS upon loading the page if one is pre-selected
+    if (heart1.checked) {
+        heart1.style.webkitAppearance = 'none';
+        heart1.style.background = 'url("/images/heart-icon-color.png")';
+        heart1.style.backgroundRepeat = 'no-repeat';
+        heart1.style.backgroundSize = '30px';
+        heart1.style.border = 'none';
+        heart1.style.height = '30px';
+        heart1.style.width = '30px';
+    } else if (heart2.checked) {
+        heart1.style.webkitAppearance = 'none';
+        heart1.style.background = 'url("/images/heart-icon-color.png")';
+        heart1.style.backgroundRepeat = 'no-repeat';
+        heart1.style.backgroundSize = '30px';
+        heart1.style.border = 'none';
+        heart1.style.height = '30px';
+        heart1.style.width = '30px';
+
+        heart2.style.webkitAppearance = 'none';
+        heart2.style.background = 'url("/images/heart-icon-color.png")';
+        heart2.style.backgroundRepeat = 'no-repeat';
+        heart2.style.backgroundSize = '30px';
+        heart2.style.border = 'none';
+        heart2.style.height = '30px';
+        heart2.style.width = '30px';
+    } else if (heart3.checked) {
+        heart1.style.webkitAppearance = 'none';
+        heart1.style.background = 'url("/images/heart-icon-color.png")';
+        heart1.style.backgroundRepeat = 'no-repeat';
+        heart1.style.backgroundSize = '30px';
+        heart1.style.border = 'none';
+        heart1.style.height = '30px';
+        heart1.style.width = '30px';
+
+        heart2.style.webkitAppearance = 'none';
+        heart2.style.background = 'url("/images/heart-icon-color.png")';
+        heart2.style.backgroundRepeat = 'no-repeat';
+        heart2.style.backgroundSize = '30px';
+        heart2.style.border = 'none';
+        heart2.style.height = '30px';
+        heart2.style.width = '30px';
+
+        heart3.style.webkitAppearance = 'none';
+        heart3.style.background = 'url("/images/heart-icon-color.png")';
+        heart3.style.backgroundRepeat = 'no-repeat';
+        heart3.style.backgroundSize = '30px';
+        heart3.style.border = 'none';
+        heart3.style.height = '30px';
+        heart3.style.width = '30px';
+    } else if (heart4.checked) {
+        heart1.style.webkitAppearance = 'none';
+        heart1.style.background = 'url("/images/heart-icon-color.png")';
+        heart1.style.backgroundRepeat = 'no-repeat';
+        heart1.style.backgroundSize = '30px';
+        heart1.style.border = 'none';
+        heart1.style.height = '30px';
+        heart1.style.width = '30px';
+
+        heart2.style.webkitAppearance = 'none';
+        heart2.style.background = 'url("/images/heart-icon-color.png")';
+        heart2.style.backgroundRepeat = 'no-repeat';
+        heart2.style.backgroundSize = '30px';
+        heart2.style.border = 'none';
+        heart2.style.height = '30px';
+        heart2.style.width = '30px';
+
+        heart3.style.webkitAppearance = 'none';
+        heart3.style.background = 'url("/images/heart-icon-color.png")';
+        heart3.style.backgroundRepeat = 'no-repeat';
+        heart3.style.backgroundSize = '30px';
+        heart3.style.border = 'none';
+        heart3.style.height = '30px';
+        heart3.style.width = '30px';
+
+        heart4.style.webkitAppearance = 'none';
+        heart4.style.background = 'url("/images/heart-icon-color.png")';
+        heart4.style.backgroundRepeat = 'no-repeat';
+        heart4.style.backgroundSize = '30px';
+        heart4.style.border = 'none';
+        heart4.style.height = '30px';
+        heart4.style.width = '30px';
+    } else if (heart5.checked) {
+        heart1.style.webkitAppearance = 'none';
+        heart1.style.background = 'url("/images/heart-icon-color.png")';
+        heart1.style.backgroundRepeat = 'no-repeat';
+        heart1.style.backgroundSize = '30px';
+        heart1.style.border = 'none';
+        heart1.style.height = '30px';
+        heart1.style.width = '30px';
+
+        heart2.style.webkitAppearance = 'none';
+        heart2.style.background = 'url("/images/heart-icon-color.png")';
+        heart2.style.backgroundRepeat = 'no-repeat';
+        heart2.style.backgroundSize = '30px';
+        heart2.style.border = 'none';
+        heart2.style.height = '30px';
+        heart2.style.width = '30px';
+
+        heart3.style.webkitAppearance = 'none';
+        heart3.style.background = 'url("/images/heart-icon-color.png")';
+        heart3.style.backgroundRepeat = 'no-repeat';
+        heart3.style.backgroundSize = '30px';
+        heart3.style.border = 'none';
+        heart3.style.height = '30px';
+        heart3.style.width = '30px';
+
+        heart4.style.webkitAppearance = 'none';
+        heart4.style.background = 'url("/images/heart-icon-color.png")';
+        heart4.style.backgroundRepeat = 'no-repeat';
+        heart4.style.backgroundSize = '30px';
+        heart4.style.border = 'none';
+        heart4.style.height = '30px';
+        heart4.style.width = '30px';
+
+        heart5.style.webkitAppearance = 'none';
+        heart5.style.background = 'url("/images/heart-icon-color.png")';
+        heart5.style.backgroundRepeat = 'no-repeat';
+        heart5.style.backgroundSize = '30px';
+        heart5.style.border = 'none';
+        heart5.style.height = '30px';
+        heart5.style.width = '30px';
+    }
+
+    // Change background of all previous hearts on hover on new review form
     const hoverOverHeart = (e) => {
         if (e.target.id === 'rating1') {
             e.target.style.webkitAppearance = 'none';
@@ -218,7 +341,7 @@ window.addEventListener('DOMContentLoaded', (e) => {
     heart5.addEventListener('mouseenter', hoverOverHeart);
 
 
-    // Remove background from all hearts when exit hover on new review form
+    // Remove background from hearts when exit hover on new review form
     const leaveHeart = (e) => {
         if (heart1.checked) {
             heart1.style.webkitAppearance = 'none';
